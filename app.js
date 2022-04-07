57d26d4c85d8b4f39022712560cedf4275d94feb
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-07 23:11:24 +0900

    에라토스테네스의 체

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
-const solution = (x) =>
-  !(x % [0, ...x.toString()].reduce((pre, current) => pre + parseInt(current)));
+function solution(n) {
+  const arr = [];
+
+  for (let i = 1; i <= n; i++) arr.push(i);
+
+  for (let i = 1; i * i < n; i++) {
+    if (arr[i]) {
+      const num = arr[i];
+      for (let k = num * num; k <= n; k += num) {
+        arr[k - 1] = 0;
+      }
+    }
+  }
+
+  const answer = arr.filter((number) => number);
+  answer.shift();
+
+  return answer.length;
+}
 
 console.log(solution(10));
-console.log(solution(12));
-console.log(solution(11));
-console.log(solution(13));
+console.log(solution(5));
