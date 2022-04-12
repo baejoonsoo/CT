0f90946c554a1e254896194ac908117810b5637f
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-04-12 22:27:29 +0900

    더 짧은 코드

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-var solution = (participant, completion) => {
-  completion.map((name) => {
-    console.log(completion);
-    return (completion[name] = (completion[name] | 0) + 1);
-  });
-  return participant.find((name) => !completion[name]--);
-};
+const numStr = [
+  "zero",
+  "one",
+  "two",
+  "three",
+  "four",
+  "five",
+  "six",
+  "seven",
+  "eight",
+  "nine",
+];
 
-console.log(solution(["leo", "kiki", "eden"], ["eden", "kiki"]));
-console.log(
-  solution(
-    ["marina", "josipa", "nikola", "vinko", "filipa"],
-    ["josipa", "filipa", "marina", "nikola"]
-  )
-);
-console.log(
-  solution(["mislav", "stanko", "mislav", "ana"], ["stanko", "ana", "mislav"])
-);
+function solution(s) {
+  numStr.forEach((num, index) => {
+    const arr = s.split(num);
+    s = arr.join(index);
+  });
+
+  return Number(s);
+}
+
+console.log(solution("one4seveneight"));
+console.log(solution("23four5six7"));
+console.log(solution("2three45sixseven"));
+console.log(solution("123"));
+console.log(solution("11zerozero323"));
