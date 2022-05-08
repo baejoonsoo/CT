bc11be07b519bc6d65bc7592b377b1ed4be7da81
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-05-08 21:29:31 +0900

    42889

--- app.js
+++ app.js
@@ -1,43 +1,55 @@
-const addNewBoard = (board) => {
-  const newBoard = [];
-  for (let i = 0; i < board[0].length; i++) {
-    const temp = [];
-    for (let k = 0; k < board.length; k++) {
-      if (board[k][i]) temp.unshift(board[k][i]);
-    }
-    newBoard.push(temp);
+// function solution(N, stages) {
+//   let failRate = [];
+
+//   for (let i = 0; i < N; i++) {
+//     failRate.push({
+//       stage: i + 1,
+//       arrival: 0,
+//       challenging: 0,
+//       rate: 0,
+//     });
+//   }
+
+//   stages.forEach((stage) => {
+//     for (let i = 0; i < N; i++) {
+//       if (stage >= i + 1) {
+//         failRate[i].arrival++;
+//       }
+//       if (stage === i + 1) {
+//         failRate[i].challenging++;
+//       }
+//     }
+//   });
+
+//   for (let i = 0; i < N; i++) {
+//     failRate[i].rate = failRate[i].arrival // 아무도 스테이지에 도달하지 못했을 때(= 0알 때) 에러남
+//       ? failRate[i].challenging / failRate[i].arrival
+//       : 0;
+//   }
+
+//   return failRate
+//     .sort((a, b) => {
+//       if (a.rate <= b.rate) {
+//         return 1;
+//       } else {
+//         return -1;
+//       }
+//     })
+//     .map((obj) => obj.stage);
+// }
+
+function solution(N, stages) {
+  let result = [];
+  for (let i = 1; i <= N; i++) {
+    let reach = stages.filter((x) => x >= i).length;
+    let curr = stages.filter((x) => x === i).length;
+    result.push([i, curr / reach]);
   }
-
-  return newBoard;
-};
-
-function solution(board, moves) {
-  const newBoard = addNewBoard(board);
-  let basket = [];
-  let removeCount = 0;
-
-  moves.forEach((move) => {
-    if (newBoard[move - 1].length) {
-      basket.push(newBoard[move - 1].pop());
-      if (basket[basket.length - 1] === basket[basket.length - 2]) {
-        basket = basket.slice(0, -2);
-        removeCount++;
-      }
-    }
-  });
-
-  return removeCount * 2;
+  result.sort((a, b) => b[1] - a[1]);
+  return result.map((x) => x[0]);
 }
 
-console.log(
-  solution(
-    [
-      [0, 0, 0, 0, 0],
-      [0, 0, 1, 0, 3],
-      [0, 2, 5, 0, 1],
-      [4, 2, 4, 4, 2],
-      [3, 5, 1, 3, 1],
-    ],
-    [1, 5, 3, 5, 1, 2, 1, 4]
-  )
-);
+console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]));
+console.log(solution(5, [2]));
+console.log(solution(4, [4, 4, 4, 4, 4]));
+console.log(solution(4, [1, 2, 3, 2, 1]));
