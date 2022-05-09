9b74ce987fd93ddc07d3541e0e48ee5d4f82fd00
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-05-09 22:40:01 +0900

    12945

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const solution = (s) =>
-  s
-    .toLowerCase()
-    .split(" ")
-    .map((str) => (str ? str[0].toUpperCase() + str.slice(1) : ""))
-    .join(" ");
+const solution = (n) => {
+  let memo = [0, 1, 1];
+  for (let i = 3; i <= n; i++) {
+    memo[i] = (memo[i - 1] + memo[i - 2]) % 1234567;
+  }
+  return memo[n];
+};
 
-console.log(solution("3people unFollowed me"));
-console.log(solution("for the last week"));
-console.log(solution("a a a a a a a a a a"));
-console.log(solution("a    a     a"));
+console.log(solution(2));
+console.log(solution(3));
+console.log(solution(5));
+console.log(solution(1000));
