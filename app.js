b315bafde26afdeef9b6f0a346faaa379456a65e
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-05-09 22:16:06 +0900

    12951

--- app.js
+++ app.js
@@ -1,55 +1,11 @@
-// function solution(N, stages) {
-//   let failRate = [];
+const solution = (s) =>
+  s
+    .toLowerCase()
+    .split(" ")
+    .map((str) => (str ? str[0].toUpperCase() + str.slice(1) : ""))
+    .join(" ");
 
-//   for (let i = 0; i < N; i++) {
-//     failRate.push({
-//       stage: i + 1,
-//       arrival: 0,
-//       challenging: 0,
-//       rate: 0,
-//     });
-//   }
-
-//   stages.forEach((stage) => {
-//     for (let i = 0; i < N; i++) {
-//       if (stage >= i + 1) {
-//         failRate[i].arrival++;
-//       }
-//       if (stage === i + 1) {
-//         failRate[i].challenging++;
-//       }
-//     }
-//   });
-
-//   for (let i = 0; i < N; i++) {
-//     failRate[i].rate = failRate[i].arrival // 아무도 스테이지에 도달하지 못했을 때(= 0알 때) 에러남
-//       ? failRate[i].challenging / failRate[i].arrival
-//       : 0;
-//   }
-
-//   return failRate
-//     .sort((a, b) => {
-//       if (a.rate <= b.rate) {
-//         return 1;
-//       } else {
-//         return -1;
-//       }
-//     })
-//     .map((obj) => obj.stage);
-// }
-
-function solution(N, stages) {
-  let result = [];
-  for (let i = 1; i <= N; i++) {
-    let reach = stages.filter((x) => x >= i).length;
-    let curr = stages.filter((x) => x === i).length;
-    result.push([i, curr / reach]);
-  }
-  result.sort((a, b) => b[1] - a[1]);
-  return result.map((x) => x[0]);
-}
-
-console.log(solution(5, [2, 1, 2, 6, 2, 4, 3, 3]));
-console.log(solution(5, [2]));
-console.log(solution(4, [4, 4, 4, 4, 4]));
-console.log(solution(4, [1, 2, 3, 2, 1]));
+console.log(solution("3people unFollowed me"));
+console.log(solution("for the last week"));
+console.log(solution("a a a a a a a a a a"));
+console.log(solution("a    a     a"));
