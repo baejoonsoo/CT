c8befada0ab38e049b42351cb7c725cea5dcd5aa
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-05-30 22:53:48 +0900

    42746

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
-const solution = (s) => {
-  let sets = s
-    .slice(2, -2)
-    .split("},{")
-    .map((set) => set.split(",").map((num) => parseInt(num)))
+const solution = (numbers) => {
+  const answer = numbers
     .sort((a, b) => {
-      const an = a.length;
-      const bn = b.length;
+      return (
+        parseInt(b.toString() + a.toString()) -
+        parseInt(a.toString() + b.toString())
+      );
+    })
+    .join("");
 
-      if (an > bn) {
-        return 1;
-      } else {
-        return -1;
-      }
-    });
-
-  const set = new Set();
-
-  sets.forEach((num) => num.forEach((oneN) => set.add(oneN)));
-
-  return [...set];
+  return answer[0] === "0" ? "0" : answer;
 };
 
-console.log(solution("{{2},{2,1},{2,1,3},{2,1,3,4}}", [2, 1, 3, 4]));
-console.log(solution("{{1,2,3},{2,1},{1,2,4,3},{2}}", [2, 1, 3, 4]));
-console.log(solution("{{20,111},{111}}", [111, 20]));
-console.log(solution("{{123}}", [123]));
-console.log(solution("{{4,2,3},{3},{2,3,4,1},{2,3}}", [3, 2, 4, 1]));
+console.log(solution([6, 10, 2]));
+console.log(solution([3, 30, 34, 5, 9]));
+console.log(solution([0, 0, 0]));
