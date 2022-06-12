2605dd1ee78cbf5e2481b39afa834817327f7a86
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-06-12 23:34:53 +0900

    .

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-codeup=[
-' ****  ***  ***   ***** *   * ****',
-'*     *   * *  *  *     *   * *   *',
-'*     *   * *   * *     *   * *   *',
-'*     *   * *   * ****  *   * ****',
-'*     *   * *   * *     *   * *',
-'*     *   * *  *  *     *   * *',
-" ****  ***  ***   *****  ***  *"]
-
-w,h=map(int,input().split())
-
-for s in codeup:
-  for i in range(h):
-    for k in range(len(s)):
-      print(s[k]*w,end='')
-    print('0')
+while True:
+  s=input()
+  if s=="END":
+    break
+  print(s[::-1])
