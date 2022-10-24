ac5107e4ed78bbfe2e482a0789d534f5aabd8820
BaeJoonSoo <baejoonsoo21@gmail.com>
2022-10-24 23:34:06 +0900

    120882

---
+++ 120882.js
@@ -0,0 +1,25 @@
+function solution(score) {
+  var answer = [];
+
+  return answer;
+}
+
+console.log(
+  solution([
+    [80, 70],
+    [90, 50],
+    [40, 70],
+    [50, 80],
+  ])
+);
+console.log(
+  solution([
+    [80, 70],
+    [70, 80],
+    [30, 50],
+    [90, 100],
+    [100, 90],
+    [100, 100],
+    [10, 30],
+  ])
+);
